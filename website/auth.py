20c80a5cc5d7a4d80f8318deb15e0e81cc56d2d7
EWanjau <wamwanjau@gmail.com>
2023-04-18 22:48:50 +0300

    made changes to the html file

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
     return render_template("sign_up.html")
