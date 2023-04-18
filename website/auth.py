5a49c26e029efb928fdd99f11e78e8066e6ae18f
EWanjau <wamwanjau@gmail.com>
2023-04-18 23:32:41 +0300

    added alert messages for login authentication

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -14,4 +14,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email Must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name Must be greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('PAsswords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at-least 7 characters', category='error')
+        else:
+            #add user to database
+            flash('Account Created Successfully', category='success')
     return render_template("sign_up.html")
