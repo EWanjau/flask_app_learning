212316a8a33823cfc15574feedbdbc3a617d0ae5
EWanjau <wamwanjau@gmail.com>
2023-04-19 11:31:21 +0300

    added database models for user accounts and notes objects

--- website/auth.py
+++ website/auth.py
@@ -25,7 +25,7 @@ def sign_up():
         elif len(firstName) < 2:
             flash('First name Must be greater than 2 characters', category='error')
         elif password1 != password2:
-            flash('PAsswords don\'t match.', category='error')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at-least 7 characters', category='error')
         else:
